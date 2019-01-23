b84455c848f5f6007df1af02b38a1f1edb9db9c0
tom martinson <progcat@comcast.net>
2019-01-23 11:56:02 -0600

    try two

---
+++ game.js
@@ -0,0 +1,52 @@
+function getGifs(subject) {
+    let key = 'VeJdZVGeAvG7paHQ77sdFdyKWd3NmiBR';
+    let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${subject}&limit=10`;
+    let response = "";
+    console.log("armed w/ajax");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $(".pictures").empty();
+        for (var x = 0; x < response.data.length; x++) {
+            $(".picArea").append(`
+                              <div class="pictures">
+                                    <h5>Rating: ${response.data[x].rating}</h5>
+                                    <img class="pictures" src="${response.data[x].images.original_still.url}" data-still="${response.data[x].images.original_still.url}" data-animate="${response.data[x].images.original.url}" data-state="still">
+                               </div>`);
+        }
+
+
+
+    });
+}
+function makeButtons() {
+    for (var x = 0; x < topics.length; x++) {
+        $(".buttons").append(`<button type="button" id="add" class="btn btn-primary pics">${topics[x]}</button>&nbsp`);
+    }
+}
+
+
+var topics = ["ufo", "big foot", "domino"];
+makeButtons();
+$("#addPic").click(function () {
+    $(".buttons").empty();
+    topics.push($("#addimg").val());
+    makeButtons();
+    $("#addimg").val("");
+});
+
+$(".buttons").click(function (event) {
+    getGifs(event.target.innerHTML);
+});
+
+$(document).on('click', ".pictures", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
